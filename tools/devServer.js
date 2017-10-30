822fd917e6845634721eea5a105b8ecc2ce82169
abinash <luffy.abinash@gmail.com>
2017-10-30 13:08:34 +0530

    adding missing necessary files

---
+++ tools/devServer.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import webpack from 'webpack';
+import config from './../webpack.config.dev';
+import path from 'path';
+import open from 'open';
+
+const port = 8080;
+const app = express();
+const webpackConfig = webpack(config);
+
+app.use(require('webpack-dev-middleware')(webpackConfig, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(webpackConfig));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './../src/index.html'));
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open(`http://localhost:${port}`);
+    }
+});
