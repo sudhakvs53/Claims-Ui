893cf4c0927b3840a442275f966a1535087237cb
Komatineni Sudhakar <komatinenisudhakar@Reeths-Mac.local>
2018-04-18 21:50:41 +0530

    pushing css chnages

--- src/components/claims/CreateClaimDetails.js
+++ src/components/claims/CreateClaimDetails.js
@@ -644,10 +644,12 @@ class CreateClaimDetails extends React.Component {
                                             <Col sm={2}>
                                                 Substantiation:
                                             </Col>
-                                            <FormControl
-                                                componentClass="textarea"
-                                                rows={this.state.substantiationRows}
-                                                onChange={this.handleSubstantiation}/>
+                                            <Col sm={8}>
+                                                <FormControl
+                                                    componentClass="textarea"
+                                                    rows={this.state.substantiationRows}
+                                                    onChange={this.handleSubstantiation}/>
+                                            </Col>
                                         </FormGroup>
                                         <FormGroup controlId="formHorizontalSubstantiationRows">
                                             <p>Rows:</p>
