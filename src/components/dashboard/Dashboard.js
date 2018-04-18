9c1c7e2fbfee3f81477ab5917bea62aac49195e8
Komatineni Sudhakar <komatinenisudhakar@Reeths-Mac.local>
2018-04-18 11:30:17 +0530

    pushing css chnages for create claims

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,39 +7,35 @@ import MainGrid from './mainGrid';
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { data: [] };
+        this.state = {
+            data: []
+        };
     }
 
     componentDidMount() {
-        return fetch('http://localhost:3002/get_project_claims', {
-            method: 'GET'
-        }).then((response) => {
+        return fetch('http://localhost:3002/get_project_claims', {method: 'GET'}).then((response) => {
             if (!response.ok) {
                 throw Error(response.statusText);
             }
             return response;
-        }).then((response) => response.json())
-            .then((data) => this.setState({ data }));
+        }).then((response) => response.json()).then((data) => this.setState({data}));
     }
 
     render() {
-        return (<div className='wrapper_grid'>
-            <span className='header_description'>Claims</span>
-            <div className='grid_container'>
-                <MainGrid server={this.state.data} />
+        return (
+            <div className='wrapper_grid'>
+                <span className='header_description'>Claims</span>
+                <div className='grid_container'>
+                    <MainGrid server={this.state.data}/>
+                </div>
             </div>
-        </div>
         );
     }
 }
 
-const mapStateToProps = (state) => {
-    return { claimHdrData: state.navToDetail, clmData: state.saveClaims, fetchClmData: state.fetchClaims };
-};
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-        fetchAllClaims
-    }, dispatch);
-}
+// const mapStateToProps = (state) => {     return {claimHdrData:
+// state.navToDetail, clmData: state.saveClaims, fetchClmData:
+// state.fetchClaims}; }; function mapDispatchToProps(dispatch) {     return
+// bindActionCreators({         fetchAllClaims     }, dispatch); }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default Dashboard;
